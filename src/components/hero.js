6af74bf48e3a28193048b025938d83bdd0475299
Hunter Davis <hunter@intercom.io>
2022-06-12 22:45:53 -0700

    update typeform link for DAO

--- src/components/hero.js
+++ src/components/hero.js
@@ -13,7 +13,7 @@ const Hero = () => (
             <div class="cta-container">
                 <a href="https://uinxq3fcctl.typeform.com/to/BhFTUOQ7" target="_blank" class="btn btn-cta">Collaborate with us <svg data-testid="arrow-icon" class="cta-link__arrow" width="13" height="10" viewBox="0 0 13 10" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M6 9L12 5L6 1" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path><path d="M1 5L11 5" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path></svg></a>
                 <span>or</span>
-                <a href="" class="btn btn-secondary">Apply for the DAO</a>
+                <a href="https://uinxq3fcctl.typeform.com/to/ZX0wN7UA" target="_blank" class="btn btn-secondary">Apply for the DAO</a>
             </div>
         </div>
       </div>
